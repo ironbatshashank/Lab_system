fa7cfb17f01a02284153f8fa2666ec1c2ed494de
Shashank Srivastava <ironbatshashank@gmail.com>
2026-04-02 10:49:15 +0100

    Update Vite config base path to root

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: '/Lab_system/',
-    optimizeDeps: {
-          exclude: ['lucide-react'],
-    },
+  plugins: [react()],
+  base: '/',
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 });
